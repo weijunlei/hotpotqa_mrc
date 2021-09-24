68c721e7d85c6652cdc7726abf8dd2c7ad17a756
qingfengwuyu <qingfengwuyu233@gmail.com>
2021-09-24 11:20:44 +0800

    恢复到reader正常水平

--- scripts/origin_reader/run_reader.sh
+++ scripts/origin_reader/run_reader.sh
@@ -1,18 +1,18 @@
 echo "----------------------------------------------------"
 echo "start train reader model"
 export CUDA_VISIBLE_DEVICES="0,1"
-cd ../reader_model
+cd ../../src/reader
 python -u origin_reader_model.py \
   --bert_model bert-base-uncased \
-  --output_dir ../checkpoints/qa_base_20210914_coattention \
+  --output_dir ../../data/checkpoints/qa_base_20210922_coattention \
   --model_name BertForQuestionAnsweringCoAttention \
-  --train_file ../data/hotpot_data/hotpot_train_labeled_data_v3.json \
-  --dev_file ../data/hotpot_data/hotpot_dev_distractor_v1.json \
-  --train_filter_file ../data/selector/second_hop_result/train_related.json \
-  --dev_filter_file ../data/selector/second_hop_result/dev_related.json \
+  --train_file ../../data/hotpot_data/hotpot_train_labeled_data_v3.json \
+  --dev_file ../../data/hotpot_data/hotpot_dev_distractor_v1.json \
+  --train_filter_file ../../data/selector/second_hop_related_paragraph_result/train_related.json \
+  --dev_filter_file ../../data/selector/second_hop_related_paragraph_result/dev_related.json \
   --use_file_cache \
-  --feature_cache_path ../data/cache/reader_cache_0607_coattention \
-  --train_batch_size 32 \
+  --feature_cache_path ../../data/cache/qa_base_20210922_coattention \
+  --train_batch_size 16 \
   --val_batch_size 128 \
   --save_model_step 3000 \
   --num_train_epochs 2.0
