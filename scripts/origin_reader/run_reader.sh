c36960b68cecb7ac751badbcce197adb90bcf1dd
xiaofenglei <qingfengwuyu233@gmail.com>
2021-10-08 10:43:56 +0800

    去除掉BERT第0层进行实验

--- scripts/origin_reader/run_reader.sh
+++ scripts/origin_reader/run_reader.sh
@@ -13,9 +13,9 @@ export CUDA_VISIBLE_DEVICES="0,1"
 cd ../../src/origin_reader
 python -u origin_reader_model.py \
   --bert_model bert-base-uncased \
-  --output_dir ../../data/checkpoints/qa_base_20211007_forward_wo_1_transformer \
+  --output_dir ../../data/checkpoints/qa_base_20211008_forward_wo_0_transformer \
   --model_name BertForQuestionAnsweringForward \
-  --log_prefix qa_base_20211007_forward_wo_1_transformer \
+  --log_prefix qa_base_20211008_forward_wo_0_transformer \
   --overwrite_result True \
   --train_file ../../data/hotpot_data/hotpot_train_labeled_data_v3.json \
   --dev_file ../../data/hotpot_data/hotpot_dev_labeled_data_v3.json \
