bf06d824d5990930a41927e7f33a0800ae2c36a8
qingfengwuyu <qingfengwuyu233@gmail.com>
2021-12-31 00:29:05 +0800

    增加Debert

--- deberta_stable_scripts/origin_reader/run_deberta_reader.sh
+++ deberta_stable_scripts/origin_reader/run_deberta_reader.sh
@@ -20,16 +20,16 @@ export CUDA_VISIBLE_DEVICES="0"
 cd ../../deberta_stable_src/origin_reader
 python -u origin_reader_model.py \
   --bert_model microsoft/deberta-v3-large \
-  --output_dir ../../data/checkpoints/20211230_deberta \
+  --output_dir ../../data/checkpoints/20211230_deberta_bs_8 \
   --model_name DebertaForQuestionAnsweringQANet \
-  --log_prefix 20211230_deberta \
+  --log_prefix 20211230_deberta_bs_8 \
   --overwrite_result True \
   --train_file ../../data/hotpot_data/hotpot_train_labeled_data_v3.json \
   --dev_file ../../data/hotpot_data/hotpot_dev_labeled_data_v3.json \
   --train_supporting_para_file ../../data/hotpot_data/train_golden.json \
   --dev_supporting_para_file ../../data/selector/20211217_second_hop_electra_base_just_paragraph_selector_12_value_setting_result/dev_related.json \
-  --feature_cache_path ../../data/cache/20211230_deberta \
-  --train_batch_size 12 \
+  --feature_cache_path ../../data/cache/20211230_deberta_bs_8 \
+  --train_batch_size 8 \
   --gradient_accumulation_steps 1 \
   --local_rank -1 \
   --learning_rate 2e-5 \
